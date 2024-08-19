0c1cf71223045f30d53e4259c4e87c911604de8e
MandarSankhe <mandarsankhe97@gmail.com>
2024-08-19 15:27:47 -0400

    rounded corners

--- functions/index.js
+++ functions/index.js
@@ -29,8 +29,8 @@ const mongoose = require("mongoose");
 app.use(express.static("public"));
 app.use(express.static("resources"));
 app.set("view engine", "ejs");
-app.listen(4004, () => {
-  console.log(`App listening on port 4004`);
+app.listen(4002, () => {
+  console.log(`App listening on port 4002`);
 });
 app.use(express.urlencoded({ extended: true }));
 app.use(
