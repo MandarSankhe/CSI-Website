40973694efed6fb58d58bf107735aa4720929969
MandarSankhe <mandarsankhe97@gmail.com>
2024-08-18 20:09:50 -0400

    image issue

--- functions/index.js
+++ functions/index.js
@@ -29,8 +29,8 @@ const mongoose = require("mongoose");
 app.use(express.static("public"));
 app.use(express.static("resources"));
 app.set("view engine", "ejs");
-app.listen(4015, () => {
-  console.log(`App listening on port 4015`);
+app.listen(4005, () => {
+  console.log(`App listening on port 4005`);
 });
 app.use(express.urlencoded({ extended: true }));
 app.use(
